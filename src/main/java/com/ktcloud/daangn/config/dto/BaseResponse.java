ee488c017220d021f98235d0e4363cac9287e6d7
sangwookkhu <ksw090711@gmail.com>
2026-04-22 17:38:32 +0900

    feat: config file setting

---
+++ src/main/java/com/ktcloud/daangn/config/dto/BaseResponse.java
@@ -0,0 +1,16 @@
+package com.ktcloud.daangn.config.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record BaseResponse<T>(
+        int code, //http status code
+        LocalDateTime localDateTime,
+        String message,
+        T Data
+) {
+    public static <T> BaseResponse<T> success(T body) {
+        return new BaseResponse<>(HttpStatus.OK.value(), LocalDateTime.now(), "정상", null);
+    }
+}
---
+++ src/main/java/com/ktcloud/daangn/config/exception/InvalidInputException.java
@@ -0,0 +1,16 @@
+package com.ktcloud.daangn.config.exception;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidInputException extends RuntimeException {
+
+    private final int statusCode;
+    private final String message;
+
+    public InvalidInputException(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+}
