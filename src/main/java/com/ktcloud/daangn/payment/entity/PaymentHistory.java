37bc954c2e9ad39d7050076f5d682ecd7b49b595
jaesung <barbo9510@icloud.com>
2026-05-27 12:31:00 +0900

    Refactor: 불필요한 import 제거 (#63)

--- src/main/java/com/ktcloud/daangn/payment/entity/PaymentHistory.java
+++ src/main/java/com/ktcloud/daangn/payment/entity/PaymentHistory.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import java.time.LocalDateTime;
 
