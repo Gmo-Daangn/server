f174a70c573d5274c1f86d293c1b9b3cae70ea84
rutin <dev.rutin@gmail.com>
2026-06-01 15:53:58 +0900

    채팅 통합테스트 springBootTest 옵션 변경 #65

--- src/test/java/com/ktcloud/daangn/chat/integration/ChatIntegrationTest.java
+++ src/test/java/com/ktcloud/daangn/chat/integration/ChatIntegrationTest.java
@@ -55,7 +55,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @ExtendWith(RestDocumentationExtension.class)
